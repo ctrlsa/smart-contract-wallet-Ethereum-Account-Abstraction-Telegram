0a841e94008c3aeaa6718eb99d3ab385ba10b094
Dasarath G <dasarathg68@gmail.com>
2024-11-20 13:00:02 +0530

    feat: init tempalte

---
+++ telegram-circle-usdc-test/app/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Section, Cell, Image, List } from "@telegram-apps/telegram-ui";
+
+import tonSvg from "./_assets/ton.svg";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Circle USDC</h1>
+    </>
+  );
+}
